9a1d76fdca1706a6f62cba25c49b9edfe6f045af
Sushi Chanrai <sushi@pathworks.co>
2018-06-06 17:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function logShout (string){
   console.log(string.toUpperCase());
 }
 
+function logWhisper(string){
+  
+}
+
 console.log(shout("hello"));
 console.log(whisper("GOODBYE"));
 logShout("hello");
