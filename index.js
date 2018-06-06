d1710cdaf202869de97b24d2c0891430cc78f355
Sushi Chanrai <sushi@pathworks.co>
2018-06-06 17:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function sayHiToGrandma(string){
   else if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  else if (string === "")
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
 
 
